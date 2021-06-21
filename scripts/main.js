84db6ff3e0e80510cb2bc3bcda8cbecb386de4eb
VanessaTinberus <42710894+VanessaTinberus@users.noreply.github.com>
2021-06-21 12:06:01 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
+// A snippet of code to replace Mozilla is Cool with Hello World
+ 
+// Grab the heading in the document
+const myHeading = document.querySelector('h1');
+ 
+// Replace the heading text with Hello World!
+myHeading.textContent = 'Hello world!';
+
+
 function changeColor(newColor) {
   var elem = document.getElementById('myPara');
   elem.style.color = newColor;
